14a2b51b516d8fb3ac0408eb60fefd4a7d5be808
Maurício Augusto <mrc_augusto@hotmail.com>
2024-10-23 16:41:29 -0300

    theme files structure created

---
+++ src/app/index/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+import {styles} from './styles'
+export default function Index(){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>Escolha os produtos</Text>
+      <Text style={styles.subtitle}>
+        Descubra receitas baseadas nos produtos que você escolheu
+      </Text>
+    </View>
+  )
+}
